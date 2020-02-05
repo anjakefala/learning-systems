6e17a6a223977faca061ee90469bf6b4acf24cfe
anjakefala <anja.kefala@gmail.com>
2020-02-04 23:01:28 -0800

    First pass of prototype

---
+++ kotekan-test/reader.c
@@ -0,0 +1,73 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
+#define BUF_SIZE 1024
+
+#define SEM_NAME "kotekan"
+#define MEM_NAME "ringBuffer"
+
+int main(int argc, char *argv[]) {
+    // Transfer blocks of data from shared memory to stdout
+    sem_t *sem;
+
+    int fd;
+    char *addr;
+    struct stat sb;
+
+    // Obtain the details of the semaphore set by the writer program
+    sem = sem_open(SEM_NAME, 0);
+    if (sem == SEM_FAILED) {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
+    }
+
+    // Open existing shared memory object
+    fd = shm_open(MEM_NAME, O_RDONLY, 0);
+
+    // Use shared memory object size as length argument for mmap()
+    // and as number of bytes to write()
+    if (fstat(fd, &sb) == -1) {
+        perror("fstat");
+        exit(EXIT_FAILURE);
+    }
+
+    // Attach the shared memory segment for read-only access
+    addr = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    if (addr == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    // fd is no longer needed
+    if (close(fd) == -1) {
+        perror("close");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        // Reserve (decrement) the lock
+        // should the reader lock?
+        if (sem_wait(sem) == -1) {
+            perror("sem_wait");
+            exit(EXIT_FAILURE);
+        }
+
+        // Write the block of data in the shared memory segment to standard output
+        write(STDOUT_FILENO, addr, sb.st_size);
+        printf("\n");
+
+        // Release (increment) the writer semaphore
+        if (sem_post(sem) == -1) {
+            perror("sem_post");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
---
+++ kotekan-test/writer.c
@@ -0,0 +1,101 @@
+#include <sys/types.h>  // Type definitions used by many programs
+#include <stdio.h>      // Standard I/O functions
+#include <stdlib.h>     // Commonly used library functions + EXIT_SUCCESS, EXIT_FAILURE
+#include <unistd.h>     // System calls
+#include <string.h>     // String-handling functions
+
+#include <sys/mman.h>   // mmap
+#include <semaphore.h>  // semaphores
+#include <fcntl.h>      // file descriptors
+
+#define BUF_SIZE 1024   // Size of transfer buffer
+
+#define SEM_NAME "kotekan"
+#define MEM_NAME "ringBuffer"
+
+int main(int argc, char *argv[]) {
+    sem_t *sem;
+    unsigned int sem_value = 1;
+    int flags = O_CREAT;
+    int perms = (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+
+    int fd;
+    size_t len = BUF_SIZE;
+    char *addr;
+
+    // 1. Create the semaphore that is being used by the writer and reader
+    // the writer should have first access
+    sem = sem_open(SEM_NAME, flags, perms, sem_value);
+    if (sem == SEM_FAILED) {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sem_wait(sem) == -1) {
+        perror("sem_wait");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Wait succeeded!\n");
+
+    // 2. Create the shared memory segment and attach it to the writer's
+    // virtual address space at an address chosen by the system
+    fd = shm_open(MEM_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        perror("shm_open");
+        exit(EXIT_FAILURE);
+    }
+
+    // Resize object to hold buffer
+    if (ftruncate(fd, len) == -1) {
+        perror("ftruncate");
+        exit(EXIT_FAILURE);
+    }
+    printf("Resized to %ld bytes\n", (long) len);
+
+    addr = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (addr == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    // 'fd' is no longer needed
+    if (close(fd) == -1) {
+        perror("close");
+        exit(EXIT_FAILURE);
+    }
+
+    // Enter a loop that transfers data to the shared
+    // memory segment.
+    while(1) {
+        char c = 'a';
+
+        for (int i = 0; i < BUF_SIZE; i++ ) {
+
+            //  - Transfer character into the shared memory segment
+            memcpy(addr+i, "W", 1);
+
+            //  - Release (increment) the lock
+            if (sem_post(sem) == -1) {
+                perror("sem_post");
+                exit(EXIT_FAILURE);
+            }
+            sleep(1);
+
+            //  - Reserve (decrement) the lock
+            if (sem_wait(sem) == -1) {
+                perror("sem_wait");
+                exit(EXIT_FAILURE);
+            }
+
+            //  - Transfer character into the shared memory segment
+            memcpy(addr+i, &c, 1);
+
+            sleep(1);
+            c++;
+        }
+    }
+    printf("Done!\n");
+    exit(EXIT_SUCCESS);
+
+}
